a76f9d34b7e69fd23fac8c50227638f8c3afad54
Raju <git.dev.raju@gmail.com>
2017-07-07 12:12:02 +0530

    use component in multiple activities

--- app/src/main/java/raju/dev/dagger2/MainActivity.java
+++ app/src/main/java/raju/dev/dagger2/MainActivity.java
@@ -1,5 +1,6 @@
 package raju.dev.dagger2;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -39,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     TextView tv_response;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.btnSecondActivity)
+    Button btnSecondActivity;
 
 
     @Override
@@ -106,6 +110,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
         updateCounter();
+
+        btnSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
 
     }
 
---
+++ app/src/main/java/raju/dev/dagger2/SecondActivity.java
@@ -0,0 +1,61 @@
+package raju.dev.dagger2;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Inject  //Uncommenting This line will not Inject DI.. Please Try this case..
+            SharedPreferences mSharedPreferences;//Sit back relax
+
+    @BindView(R.id.tv_launchCounter)
+    TextView tv_font;
+    @BindView(R.id.response)
+    TextView tv_response;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        ((MyApp) getApplication()).getGitHubComponent().inject(this);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        updateCounter();
+
+    }
+
+    private String updateCounter() {
+        if (mSharedPreferences != null) {
+            tv_response.setText("SharedPreferences Dependency Injection Worked!");
+        } else {
+            tv_response.setText(getString(R.string.sfnotworking));
+            tv_font.setText("Oops! Something went wrong..");
+            return getString(R.string.sfnotworking);
+        }
+        int countTemp = mSharedPreferences.getInt("launch_counter", 0);
+        int counter = countTemp + 1;
+
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putInt("launch_counter", counter);
+        editor.commit();
+
+        tv_font.setText("You've Got " + counter + " views");
+
+        return "SharedPreferences Dependency Injection Worked!";
+    }
+
+
+}
