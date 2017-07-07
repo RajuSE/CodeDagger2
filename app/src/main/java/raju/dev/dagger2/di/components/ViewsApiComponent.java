f5df4f870bb4d1f8c889872dbc36517ddfc8767c
Raju <git.dev.raju@gmail.com>
2017-07-07 16:33:09 +0530

    added missing scope

--- app/src/main/java/raju/dev/dagger2/di/components/ViewsApiComponent.java
+++ app/src/main/java/raju/dev/dagger2/di/components/ViewsApiComponent.java
@@ -5,7 +5,9 @@ import dagger.Component;
 import raju.dev.dagger2.MainActivity;
 import raju.dev.dagger2.SecondActivity;
 import raju.dev.dagger2.di.modules.ViewsApiModule;
+import raju.dev.dagger2.di.scopes.UserScope;
 
+@UserScope
 @Component(dependencies = HttpComponent.class, modules = ViewsApiModule.class)
 public interface ViewsApiComponent {
     void inject(MainActivity activity);//TODO Note: Putting Activity willbe Failed because Dagger2 needs to know the concrete classes that will be injected at COMPILE time, not runtime (like Dagger 1 could do).
